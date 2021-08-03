1fe4dd2a5c6d6f3ae2c943b9c81615c8d766b5b0
Opeyem Peter <opeyemiadebowale1759@gmail.com>
2021-08-03 15:28:26 +0100

    folder structure

---
+++ src/Components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Logo.css";
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <span className="food">Food</span>
+      <span className="bag">Bag</span>
+    </div>
+  );
+};
+
+export default Logo;
