80c0504d4e797d44ae5158131550ed507d34543f
Opeyem Peter <opeyemiadebowale1759@gmail.com>
2021-08-03 15:56:48 +0100

    responsive nav bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Logo from "./Components/Logo/Logo";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
-  return
-  <>
-
-
-  </>;
+  return (
+    <div className="">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './Navbar.css'
+import Logo from "../Logo/Logo";
+const Navbar = () => {
+  return (
+    <div className="foodbag-navbar">
+      <div>
+        <Logo />
+      </div>
+      <ul className="foodbag-nav-menu">
+        <li className="foodbag-nav-item">Cart</li>
+        <li className="foodbag-nav-item">Login</li>
+        <li className="foodbag-nav-item signup-item">Sign Up</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
