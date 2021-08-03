cfbc4d6fd9bbd2c2bc6dcaed4e08577edbab3539
Opeyem Peter <opeyemiadebowale1759@gmail.com>
2021-08-03 21:52:29 +0100

    pick interest

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Logo from "./Components/Logo/Logo";
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Pages/home/Home";
 
