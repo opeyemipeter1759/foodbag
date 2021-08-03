edc977b158a5a0ea079da67abab9cc76a8a4bd6d
Opeyem Peter <opeyemiadebowale1759@gmail.com>
2021-08-04 00:12:32 +0100

    pick interest

--- src/Components/PickInterest/PickInterest.js
+++ src/Components/PickInterest/PickInterest.js
@@ -1,12 +1,15 @@
 import React from "react";
+import "./PickInterest.css";
 
 const PickInterest = ({ foodImg, category, description }) => {
   return (
     <div className="pick-interest">
-      <img src={foodImg} alt="Food" />
-      <div className="pick-interest-category">
-        <h3>{category}</h3>
-        <p>{description}</p>
+      <div>
+        <img src={foodImg} alt="Food" />
+        <div className="pick-interest-category">
+          <h3>{category}</h3>
+          <p>{description}</p>
+        </div>
       </div>
     </div>
   );
--- src/Pages/home/HeroSession.js
+++ src/Pages/home/HeroSession.js
@@ -7,6 +7,10 @@ import fastFood from "../../Assets/Images/fast-food.svg";
 import drinkCocktail from "../../Assets/Images/Drinks & Cocktails.svg";
 import resturant from "../../Assets/Images/resturant.svg";
 import vegeFruit from "../../Assets/Images/Vegetables-Drinks.svg";
+import illustrate1 from "../../Assets/Images/Illustration_1.svg";
+import illustrate2 from "../../Assets/Images/illustration_2.svg";
+import illustrate3 from "../../Assets/Images/illustration_3.svg";
+
 const HeroSession = () => {
   return (
     <>
@@ -23,29 +27,56 @@ const HeroSession = () => {
 
       <div className="pick-interest-details">
         <p>Pick an interest</p>
-        <PickInterest
-          foodImg={fastFood}
-          category="Fast Food"
-          description="All sorrows are less with bread"
-        />
 
-        <PickInterest
-          foodImg={vegeFruit}
-          category="Fast Food"
-          description="All sorrows are less with bread"
-        />
+        <div className="pick-interest-section">
+          <PickInterest
+            foodImg={fastFood}
+            category="Fast Food"
+            description="All sorrows are less with bread"
+          />
 
-        <PickInterest
-          foodImg={drinkCocktail}
-          category="Fast Food"
-          description="All sorrows are less with bread"
-        />
-        <PickInterest
-          foodImg={resturant}
-          category="Fast Food"
-          description="All sorrows are less with bread"
-        />
+          <PickInterest
+            foodImg={vegeFruit}
+            category="Vegetables & Fruits"
+            description="May not want it, but good for you"
+          />
+
+          <PickInterest
+            foodImg={drinkCocktail}
+            category="Drink & Cocktails"
+            description="I feel sad for those who don't drink it"
+          />
+          <PickInterest
+            foodImg={resturant}
+            category="Resturants"
+            description="All sorrows are less with bread"
+          />
+        </div>
       </div>
+
+      <hr />
+      <div className="how-we-work">
+        <p>How we serve you</p>
+
+        <div className="how-we-work-body">
+          <div>
+            <img src={illustrate1} alt="illustration" />
+            <h3>Tell us where you are</h3>
+            <p>Select the location you want us to deliver.</p>
+          </div>
+          <div>
+            <img src={illustrate2} alt="illustration" />
+            <h3>Tell us what you want</h3>
+            <p>Browse the typeof food that interest you.</p>
+          </div>
+          <div>
+            <img src={illustrate3} alt="illustration" />
+            <h3>We'll come running</h3>
+            <p>Your order will be delivered to you in no time.</p>
+          </div>
+        </div>
+      </div>
+      <hr />
     </>
   );
 };
